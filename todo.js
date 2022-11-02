44d73647acf392d269db57bda1357d68beb79ddb
nrgbrva <hanbarovan@yandex.az>
2022-11-02 11:15:31 +0400

    second

--- todo.js
+++ todo.js
@@ -1,5 +1,6 @@
 let input=document.querySelector('input');
 let button=document.querySelector('.btn')
+let notes=document.querySelector('.notes')
 let app=document.querySelector('.app')
 button.addEventListener('click',function(x){
     //button.style.display='none'
@@ -8,7 +9,12 @@ button.addEventListener('click',function(x){
 
     newNote.innerText=input.value;
     newNote.setAttribute('class','input-part')
-    newNote.style.background='red'
-    app.append(newNote)
+    
+    notes.append(newNote)
     console.log(input.value)
+})
+let del=document.querySelector('.input-part img')
+del.addEventListener('click',function(x){
+    x.preventDefault();
+    
 })
